642a8de62604fdf09ef8c19363dbfaf7631cb272
Vishal09090 <Vishalsnagave1234@gmail.com>
2023-11-17 23:17:40 +0530

    add routes

---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginSignup() {
+  return (
+    <div>LoginSignubp</div>
+  )
+}
+
+export default LoginSignup
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../Components/HERO/Hero'
+
+function Shop() {
+  return (
+    <div>
+      <Hero/>
+      Shop
+       
+    </div>
+  )
+}
+
+export default Shop
